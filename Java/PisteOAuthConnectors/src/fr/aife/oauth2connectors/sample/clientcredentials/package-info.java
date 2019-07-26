a2cd00ee17f25118d072a055a971a8933a2b51d1
Sokhom CHAU <admin-plateforme-api.aife@finances.gouv.fr>
2019-07-26 16:40:48 +0200

    PisteOAuthConnectors Java Package V1.0

---
+++ Java/PisteOAuthConnectors/src/fr/aife/oauth2connectors/sample/clientcredentials/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package helps you to develop your kit to generate an OAuth2.0 accessToken Client 
+ * Credentials with PISTE then get a ressource protected with this token.
+ * This package needs  
+ */
+/**
+ * @author AIFE
+ *
+ */
+package fr.aife.oauth2connectors.sample.clientcredentials;
